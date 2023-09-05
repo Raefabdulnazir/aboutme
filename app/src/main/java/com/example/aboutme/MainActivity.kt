9b07e6beb3b91d8fe81141e79e6cb3ad80c47355
Raefabdulnazir <raefnazir@gmail.com>
2023-09-05 22:14:17 +0400

    About me application

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -10,8 +10,10 @@ import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var myName : MyName = MyName("RAEF")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.myName = myName
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         //findViewById<Button>(R.id.done_button).setOnClickListener{
@@ -28,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         //val nicknameTextView = findViewById<TextView>(R.id.nickname_textview)
         binding.apply {
             nicknameTextview.text = nicknameEdit.text
+            myName?.nickname=nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             view.visibility = View.GONE
